f18953ab340d007d10f94e40816422010056d96e
AlbanChatelet <chateletalban20@gmail.com>
2025-03-18 14:46:19 +0100

    menu nickel

---
+++ src/assets/icons/MenuBurger.vue
@@ -0,0 +1,9 @@
+<template>
+    <svg width="39" height="27" viewBox="0 0 39 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 13.5H19.5H39" stroke="#CCAA5E" stroke-width="4"/>
+<path d="M0 2H19.5H39" stroke="#CCAA5E" stroke-width="4"/>
+<path d="M0 25H19.5H39" stroke="#CCAA5E" stroke-width="4"/>
+</svg>
+
+
+</template>
